a6339dc8ec5f2c21d23ef6668c172ecc166133a0
= <augustodemeloh@protonmail.com>
2025-07-12 23:46:29 -0300

    feat: add image compressing before updating profile photo

--- app/Actions/Fortify/UpdateUserProfileInformation.php
+++ app/Actions/Fortify/UpdateUserProfileInformation.php
@@ -3,9 +3,11 @@
 namespace App\Actions\Fortify;
 
 use App\Models\User;
+use Illuminate\Validation\Rule;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Validation\Rule;
+use Intervention\Image\Laravel\Facades\Image;
 use Laravel\Fortify\Contracts\UpdatesUserProfileInformation;
 
 class UpdateUserProfileInformation implements UpdatesUserProfileInformation
@@ -17,6 +19,23 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
      */
     public function update(User $user, array $input): void
     {
+        if (isset($input['photo'])) {
+            $processedImage = $input['photo'] = Image::read($input['photo'])
+                ->scale(250, 250)
+                ->toJpeg();
+
+            $imageTempPath = tempnam(sys_get_temp_dir(), 'profile_photo') . '.jpg';
+            file_put_contents($imageTempPath, (string) $processedImage);
+
+            $input['photo'] = new UploadedFile(
+                $imageTempPath,
+                'profile_photo.jpg',
+                'image/jpeg',
+                null,
+                true
+            );
+        }
+
         Validator::make($input, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'email', 'max:255', Rule::unique('users')->ignore($user->id)],
@@ -25,6 +44,7 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
 
         if (isset($input['photo'])) {
             $user->updateProfilePhoto($input['photo']);
+            unlink($imageTempPath);
         }
 
         if ($input['email'] !== $user->email &&
