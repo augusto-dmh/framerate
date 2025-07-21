11b61822808d015436a098ef691d9f367d8dd9e9
= <augustodemeloh@protonmail.com>
2025-07-21 19:59:25 -0300

    feat: add base for posts listing

---
+++ resources/js/Pages/Posts/Index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import Container from '@/Components/Container.vue';
+import AppLayout from '@/Layouts/AppLayout.vue';
+
+defineOptions({
+    layout: AppLayout,
+});
+
+defineProps(['posts']);
+</script>
+
+<template>
+    <Container>
+        <ul class="divide-y">
+            <li v-for="post in posts" class="px-2 py-4">
+                <span class="font-bold text-lg">{{ post.title }}</span>
+            </li>
+        </ul>
+    </Container>
+</template>
