b7557b38d923676aad11accadaed92b3972db5d3
= <augustodemeloh@protonmail.com>
2025-07-29 20:34:53 -0300

    feat: add post show

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -42,7 +42,11 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        //
+        $post->load('user');
+
+        return Inertia::render('Posts/Show', [
+            'post' => PostResource::make($post),
+        ]);
     }
 
     /**
