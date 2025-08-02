53e8fc5e546391c20814fb01058c14c47678ba1b
= <augustodemeloh@protonmail.com>
2025-08-02 01:15:41 -0300

    test: adapt tests to comments in posts

--- tests/Feature/Controllers/PostController/ShowTest.php
+++ tests/Feature/Controllers/PostController/ShowTest.php
@@ -3,6 +3,10 @@
 use App\Models\Post;
 use function Pest\Laravel\get;
 use App\Http\Resources\PostResource;
+use App\Models\Comment;
+use App\Http\Resources\CommentResource;
+use Inertia\Testing\AssertableInertia;
+use App\Models\User;
 
 it('can show a post', function () {
     $post = Post::factory()->create();
@@ -12,10 +16,23 @@ it('can show a post', function () {
 });
 
 it('passes a post to the view', function () {
-    $post = Post::factory()
-        ->create()
-        ->load('user');
+    $user = User::factory()->create();
+    $post = Post::factory()->for($user)->create();
+    Comment::factory(2)->for($post)->create();
+
+    $post->load('user');
 
     get(route('posts.show', $post))
         ->assertHasResource('post', PostResource::make($post));
+});
+
+it('passes comments to the view', function () {
+    $post = Post::factory()->create();
+    $comments = Comment::factory(2)->for($post)->create()->reverse();
+
+    $comments->load('user');
+
+    get(route('posts.show', $post))
+        ->assertOk()
+        ->assertHasPaginatedResource('comments', CommentResource::collection($comments));
 });
