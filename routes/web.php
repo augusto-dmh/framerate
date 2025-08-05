ce296cb2514754c84b880b7b21cbd30b1fb6d9fa
= <augustodemeloh@protonmail.com>
2025-08-05 01:41:02 -0300

    feat: add comments creation

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\CommentController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -26,3 +27,4 @@ Route::middleware([
 
 Route::get('posts', [PostController::class, 'index'])->name('posts.index');
 Route::get('posts/{post}', [PostController::class, 'show'])->name('posts.show');
+Route::post('posts/{post}/comments', [CommentController::class, 'store'])->name('posts.comments.store');
