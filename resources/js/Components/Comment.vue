a7388255fe89e4554555460ac8b8b2065650706b
= <augustodemeloh@protonmail.com>
2025-08-09 14:07:19 -0300

    feat: add frontend commenting

--- resources/js/Components/Comment.vue
+++ resources/js/Components/Comment.vue
@@ -14,7 +14,7 @@ import { formatDate } from './Utilities/date';
     <div class="flex items-center gap-4">
         <img :src="comment.user.profile_photo_url" :alt="comment.user.name" class="rounded-ful w-12 h-12" />
         <div class="flex gap-2 flex-col">
-            <span class="text-sm" v-html="preventWidow(comment.body)"></span>
+            <span class="text-sm break-all" v-html="preventWidow(comment.body)"></span>
             <span class="text-xs text-gray-600 block">By {{ comment.user.name }} {{ formattedDate(comment.created_at) }} ago</span>
         </div>
     </div>
--- resources/js/Pages/Posts/Show.vue
+++ resources/js/Pages/Posts/Show.vue
@@ -7,6 +7,19 @@
 
             <div class="mt-12 mt-4">
                 <h2 class="text-xl font-semibold">Comments</h2>
+
+                <form @submit.prevent="addComment">
+                    <div>
+                        <InputLabel for="body" class="sr-only">Comment</InputLabel>
+                        <TextArea id="body" v-model="commentForm.body" />
+                        <InputError :message="commentForm.errors.body" />
+                    </div>
+
+                    <PrimaryButton type="submit" class="mt-3" :disabled="commentForm.processing">
+                        Add comment
+                    </PrimaryButton>
+                </form>
+
                 <ul class="divide-y">
                     <li v-for="comment in comments.data" class="flex gap-4 px-2 py-4">
                         <Comment :comment="comment" />
@@ -22,12 +35,32 @@
 <script setup>
 import Comment from '@/Components/Comment.vue';
 import Container from '@/Components/Container.vue';
+import InputError from '@/Components/InputError.vue';
+import InputLabel from '@/Components/InputLabel.vue';
 import Pagination from '@/Components/Pagination.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import TextArea from '@/Components/TextArea.vue';
 import { formatDate } from '@/Components/Utilities/date';
 import AppLayout from '@/Layouts/AppLayout.vue';
+import { useForm } from '@inertiajs/vue3';
 import { computed } from 'vue';
 
 const props = defineProps(['post', 'comments']);
 
 const postDateFormatted = computed(() => formatDate(props.post.created_at));
+
+const commentForm = useForm({
+    'body': '',
+});
+
+const addComment = () => {
+    commentForm.post(
+        route('posts.comments.store', props.post.id),
+        {
+            preserveScroll: true,
+            onSuccess: () => commentForm.body = '',
+        },
+    );
+};
+
 </script>
