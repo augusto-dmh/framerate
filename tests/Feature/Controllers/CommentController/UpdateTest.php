223593d307fc37ca87423481612fdeaad2016355
= <augustodemeloh@protonmail.com>
2025-08-16 01:31:16 -0300

    test: add tests to implement updating comment feature

---
+++ tests/Feature/Controllers/CommentController/UpdateTest.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Models\Comment;
+
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\assertDatabaseHas;
+use function Pest\Laravel\put;
+use App\Models\User;
+
+it('requires authentication', function () {
+    $comment = Comment::factory()->create();
+
+    put(route('comments.update', $comment))
+        ->assertRedirect(route('login'));
+});
+
+it('can update a comment', function() {
+   $comment = Comment::factory()->create();
+
+   actingAs($comment->user)->put(route('comments.update', $comment), ['body' => 'some body']);
+
+   assertDatabaseHas(Comment::class, ['body' => 'some body']);
+});
+
+it('redirects to the post show page', function() {
+    $user = User::factory()->create();$comment = Comment::factory()->create();
+
+    actingAs($comment->user)->put(route('comments.update', $comment))
+        ->assertRedirect(route('posts.show', $comment->post_id));
+});
+
+it('redirects to the correct page of comments', function () {
+    $comment = Comment::factory()->create();
+
+    actingAs($comment->user)->put(route('comments.update', ['comment' => $comment, 'page' => 10]))
+        ->assertRedirect(route('posts.show', ['post' => $comment->post_id, 'page' => 10]));
+});
+
+it('cannot update a comment from another user', function () {
+    /** @var \App\Models\User $nonAuthor */
+    $nonAuthor = User::factory()->create();
+    $comment = Comment::factory()->create();
+
+    actingAs($nonAuthor)->put(route('comments.update', $comment))
+        ->assertForbidden();
+});
