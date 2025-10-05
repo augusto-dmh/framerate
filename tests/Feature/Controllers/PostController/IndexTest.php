0fb2bc0b38df2642e3d2816182357f80865c4343
= <augustodemeloh@protonmail.com>
2025-10-05 14:50:38 -0300

    test: add test on posts listing filtered by topic

--- tests/Feature/Controllers/PostController/IndexTest.php
+++ tests/Feature/Controllers/PostController/IndexTest.php
@@ -4,6 +4,8 @@ use Inertia\Testing\AssertableInertia;
 use function Pest\Laravel\get;
 use App\Models\Post;
 use App\Http\Resources\PostResource;
+use App\Models\Topic;
+use App\Http\Resources\TopicResource;
 
 it('should return the correct component', function () {
     $this->withoutExceptionHandling();
@@ -20,4 +22,22 @@ it('passes posts to the view', function () {
 
     get(route('posts.index'))
         ->assertHasPaginatedResource('posts', PostResource::collection($posts));
+});
+
+it('can filter to a topic', function () {
+    $general = Topic::factory()->create();
+    $posts = Post::factory(2)->for($general)->create();
+    $otherPosts = Post::factory(3)->create();
+
+    $posts->load(['user', 'topic']);
+
+    get(route('posts.index', ['topic' => $general]))
+        ->assertHasPaginatedResource('posts', PostResource::collection($posts->reverse()));
+});
+
+it('passes the selected topic to the view', function () {
+    $topic = Topic::factory()->create();
+
+    get(route('posts.index', ['topic' => $topic]))
+        ->assertHasResource('selectedTopic', TopicResource::make($topic));
 });
