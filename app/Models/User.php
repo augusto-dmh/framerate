b94ad7c09bf972c0c16bd7850ae7eaf9cf3d4741
= <augustodemeloh@protonmail.com>
2025-10-11 18:48:30 -0300

    chore: rm laravel-sanctum

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,12 +9,9 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
-    use HasApiTokens;
-
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory;
     use HasProfilePhoto;
