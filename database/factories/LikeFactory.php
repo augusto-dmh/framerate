8ca52f166ddfcb37b9e56494d3c8334fbdfc77fb
= <augustodemeloh@protonmail.com>
2025-10-12 16:55:16 -0300

    feat: add LikeFactory

---
+++ database/factories/LikeFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+use App\Models\Post;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Like>
+ */
+class LikeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'likeable_type' => Post::class,
+            'likeable_id' => Post::factory(),
+        ];
+    }
+}
