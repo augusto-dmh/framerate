2a2be7a811b8ad0541aeafee36d952d761435e38
= <augustodemeloh@protonmail.com>
2025-10-18 09:01:11 -0300

    feat: add unliking to likeables

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -11,22 +11,6 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 class LikeController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request, string $type, int $id)
@@ -45,35 +29,17 @@ class LikeController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     */
-    public function show(Like $like)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Like $like)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Like $like)
-    {
-        //
-    }
-
-    /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Like $like)
+    public function destroy(Request $request, string $type, int $id)
     {
-        //
+        $likeable = $this->findLikeable($type, $id);
+        Gate::authorize('delete', [Like::class, $likeable]);
+
+        $likeable->likes()->whereBelongsTo($request->user())->delete();
+        $likeable->decrement('likes_count');
+
+        return back();
     }
 
     protected function findLikeable(string $likeableType, int $likeableId) {
